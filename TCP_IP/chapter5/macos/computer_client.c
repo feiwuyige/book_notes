a0b923dee557410ed5d3b342ad93a5ce32f44ca7
yilangzhao <yilangzhao@tencent.com>
2025-08-11 09:59:17 +0800

    修改计算器执行逻辑

--- TCP_IP/chapter5/macos/computer_client.c
+++ TCP_IP/chapter5/macos/computer_client.c
@@ -4,8 +4,10 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <memory.h>
+#include <stdint.h>
 
 #define BUFSIZE 1024
+#define OPSIZE 4
 
 void error_handling(char *message){
     fputs(message, stderr);
@@ -14,21 +16,16 @@ void error_handling(char *message){
 }
 
 int main(int argc, char *argv[]){
-    printf("%d\n", argc);
-    for(int i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-    int sock;
-    struct sockaddr_in serv_addr;
     if(argc != 3){
         printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
+    int sock;
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1){
         error_handling("sock() error");
     }
-    
+    struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
@@ -40,55 +37,23 @@ int main(int argc, char *argv[]){
     else{
         puts("Connected........");
     }
-
-    int operand_count;
+    char opmsg[BUFSIZE];
+    int32_t operand_count;
     fputs("Operand count: ", stdout);
     scanf("%d", &operand_count);
-    getchar();
-    int *operand;
-    operand = (int *)malloc(sizeof(int) * operand_count);
-    if(NULL == operand){
-        error_handling("malloc() error");
-    }
+    opmsg[0] = (char)operand_count;
+
     for(int i = 0;i < operand_count;++i){
         printf("Operand %d: ", i + 1);
-        scanf("%d", operand + i);
-        getchar();
+        scanf("%d", (int32_t *)&opmsg[i * OPSIZE + 1]);
     }
-    char operator;
+    getchar();
     fputs("Operator: ", stdout);
-    scanf("%c", &operator);
-    int i = 0;
-    for(i = 0;i < operand_count - 1; ++i){
-        printf("%d\n", *(operand+i));
-        write(sock, (char*)(operand+i), sizeof(*(operand + i)));
-        printf("%c\n", operator);
-        write(sock, &operator, sizeof(operator));
-        fputs("have written loop", stdout);
-    }
-    operator = '=';
-    fputs("have written ", stdout);
-    write(sock, (char*)(operand+i), sizeof(*(operand + i)));
-    write(sock, &operator, sizeof(operator));
-    fputs("have written ", stdout);
-    char result[BUFSIZE] = {0};
-    int used_bytes = 0;
-    int read_bytes = 0;
-    int recv_bytes = 0;
-    while((recv_bytes = read(sock, result + used_bytes, BUFSIZE)) != 0){
-        used_bytes += recv_bytes;
-        for(int j = read_bytes; j < used_bytes; ++j){
-            if(result[j] == '='){
-                //divide the message
-                char *expression_begin = result + read_bytes;
-                char *expression_end = result + j;
-                *expression_end = '\0';
-                int answer = atoi(expression_begin);
-                printf("Operation result: %d", answer);
-            }
-        }
-    }
-    free(operand);
+    scanf("%c", &opmsg[operand_count * OPSIZE + 1]);
+    write(sock, opmsg, operand_count * OPSIZE + 2);
+    int32_t result;
+    read(sock, &result, sizeof(result));
+    printf("Operation result: %d \n", result);
     close(sock);
     return 0;
 }
